18a07a27dde053def99e1a87cc268d34ece7757a
Suraj Singh <47292083+slkshatriya@users.noreply.github.com>
2025-11-18 15:24:42 +0530

    Update myscript.ps1

--- scripts/myscript.ps1
+++ scripts/myscript.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$InputMessage = "Default message",
-    [string]$Environment = "Production"
+    [string]$Environment = "Prod"
 )
 
 Write-Output "=== PowerShell Script Execution Started ==="
